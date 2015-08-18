5989195a94d51854a534f166bb2291939436db92
Rob Prouse <rob@prouse.org>
2015-08-18 17:33:21 -0400

    Building out the view models

---
+++ IdeaTrackr/IdeaTrackr/ViewModels/BaseViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdeaTrackr.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        bool _loading;
+
+        public bool Loading
+        {
+            get { return _loading; }
+            set
+            {
+                if (_loading != value)
+                {
+                    _loading = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ IdeaTrackr/IdeaTrackr/ViewModels/IdeaListViewModel.cs
@@ -0,0 +1,43 @@
+﻿using IdeaTrackr.Models;
+using System.Collections.ObjectModel;
+
+namespace IdeaTrackr.ViewModels
+{
+    public class IdeaListViewModel : BaseViewModel
+    {
+        private ObservableCollection<Idea> _ideas;
+
+        public ObservableCollection<Idea> Ideas
+        {
+            get { return _ideas; }
+            set
+            {
+                _ideas = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public void Load()
+        {
+            if (Ideas != null)
+                return;
+
+            Loading = true;
+
+            App.GetIdeaService().GetIdeas().ContinueWith(r =>
+            {
+                if(r.Exception == null)
+                {
+                    var ideaResults = r.Result;
+                    Ideas = new ObservableCollection<Idea>(ideaResults);
+                }
+                else
+                {
+                    // TODO: Raise an error
+                }
+                Loading = false;
+            });
+
+        }
+    }
+}
