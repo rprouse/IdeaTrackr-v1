432cd1720d2fa6b6f821a76028ea97a8501a4800
Rob Prouse <rob@prouse.org>
2015-08-18 16:44:10 -0400

    Simple proof that we can connect to the backend from the mobile clients

---
+++ IdeaTrackr/IdeaTrackr/Models/Idea.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdeaTrackr.Models
+{
+    public class Idea
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Problem { get; set; }
+
+        public string Solution { get; set; }
+
+        public string Notes { get; set; }
+
+        public int Rating { get; set; }
+    }
+}
---
+++ IdeaTrackr/IdeaTrackr/Views/IdeaList.xaml.cs
@@ -0,0 +1,27 @@
+﻿using IdeaTrackr.Models;
+using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace IdeaTrackr.Views
+{
+    public partial class IdeaList : ContentPage
+    {
+        public IdeaList()
+        {
+            InitializeComponent();
+        }
+
+        public async void OnButtonClicked(object sender, EventArgs e)
+        {
+            var client = new MobileServiceClient("http://localhost:60978/");
+            var ideas = await client.GetTable<Idea>().ReadAsync();
+            message.Text = ideas.First().Name;
+        }
+    }
+}
