3dd631da0a96abf437d2ceb768d48628c87f1cb3
Rob Prouse <rob@prouse.org>
2015-08-31 13:27:50 -0400

    Added authentication to the backend

--- IdeaTrackr.Backend/Controllers/IdeaController.cs
+++ IdeaTrackr.Backend/Controllers/IdeaController.cs
@@ -4,11 +4,14 @@ using System.Web.Http;
 using System.Web.Http.Controllers;
 using System.Web.Http.OData;
 using Microsoft.WindowsAzure.Mobile.Service;
+using Microsoft.WindowsAzure.Mobile.Service.Security;
 using IdeaTrackr.Backend.DataObjects;
 using IdeaTrackr.Backend.Models;
+using System.Net;
 
 namespace IdeaTrackr.Backend.Controllers
 {
+    [AuthorizeLevel(AuthorizationLevel.User)]
     public class IdeaController : TableController<Idea>
     {
         protected override void Initialize(HttpControllerContext controllerContext)
@@ -19,22 +22,41 @@ namespace IdeaTrackr.Backend.Controllers
         }
 
         // GET tables/Idea
-        public IQueryable<Idea> GetAllIdeas() => Query();
+        public IQueryable<Idea> GetAllIdeas() => Query().Where(i => i.UserId == ServiceUser.Id);
 
         // GET tables/Idea/48D68C86-6EA6-4C25-AA33-223FC9A27959
-        public SingleResult<Idea> GetIdea(string id) => Lookup(id);
+        //public SingleResult<Idea> GetIdea(string id) => Lookup(id);
 
         // PATCH tables/Idea/48D68C86-6EA6-4C25-AA33-223FC9A27959
-        public Task<Idea> PatchIdea(string id, Delta<Idea> patch) => UpdateAsync(id, patch);
+        public Task<Idea> PatchIdea(string id, Delta<Idea> patch)
+        {
+            var idea = patch.GetEntity();
+            if (idea.UserId != ServiceUser.Id)
+            {
+                throw new HttpResponseException(HttpStatusCode.Forbidden);
+            }
+            return UpdateAsync(id, patch);
+        }
 
         // POST tables/Idea
         public async Task<IHttpActionResult> PostIdea(Idea item)
         {
+            item.UserId = ServiceUser.Id;
             Idea current = await InsertAsync(item);
             return CreatedAtRoute("Tables", new { id = current.Id }, current);
         }
 
         // DELETE tables/Idea/48D68C86-6EA6-4C25-AA33-223FC9A27959
-        public Task DeleteIdea(string id) => DeleteAsync(id);
+        public async Task DeleteIdea(string id)
+        {
+            var result = await LookupAsync(id);
+            var idea = result.Queryable.FirstOrDefault();
+            if (idea.UserId == ServiceUser.Id)
+            {
+                await DeleteAsync(id);
+            }
+        }
+
+        ServiceUser ServiceUser => User as ServiceUser;
     }
 }
--- IdeaTrackr.Backend/DataObjects/Idea.cs
+++ IdeaTrackr.Backend/DataObjects/Idea.cs
@@ -14,5 +14,7 @@ namespace IdeaTrackr.Backend.DataObjects
         public string Notes { get; set; }
 
         public int Rating { get; set; }
+
+        public string UserId { get; set; }
     }
 }
